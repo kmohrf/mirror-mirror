e7974b8be65968736e684d093f0ac423c87e225d
Konrad Mohrfeldt <konrad.mohrfeldt@farbdev.org>
2017-02-28 02:14:39 +0100

    add moment and duration and calendar filter

--- src/service/server/renderer.js
+++ src/service/server/renderer.js
@@ -1,4 +1,14 @@
 import Twig from 'twig'
+import moment from 'moment'
+
+Twig.extendFilter('duration', (time, conversion = 'humanize') => {
+  const mTime = moment.duration(time)
+  return mTime[conversion]()
+})
+
+Twig.extendFilter('calendar', time => {
+  return moment(time).calendar()
+})
 
 function path(path, options) {
   return new Promise((resolve, reject) => {
