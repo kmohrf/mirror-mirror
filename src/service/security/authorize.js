4ed7b2c22c4b6139aa8324b9d07c4bd02a08f322
Konrad Mohrfeldt <konrad.mohrfeldt@farbdev.org>
2017-03-03 01:16:00 +0100

    fix indent

--- src/service/security/authorize.js
+++ src/service/security/authorize.js
@@ -2,13 +2,13 @@ export default function (User, encoder) {
   return function (email, password) {
     return new Promise((resolve, reject) => {
       return User.findOne({ where: { email } })
-                .then(user => {
-                  if (user && user.password === encoder(password, user.salt)) {
-                    resolve(user)
-                  } else {
-                    reject('Invalid email and/or password')
-                  }
-                })
+        .then(user => {
+          if (user && user.password === encoder(password, user.salt)) {
+            resolve(user)
+          } else {
+            reject('Invalid email and/or password')
+          }
+        })
     })
   }
 }
